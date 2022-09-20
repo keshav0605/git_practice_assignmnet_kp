1a009f18dd3ad8cdef745c4d7b01025f3314a5f5
unknown <mohimabahadur@gmail.com>
2022-09-20 23:19:49 +0530

    code fixed by mohima

--- prime_keshav.js
+++ prime_keshav.js
@@ -1,6 +1,13 @@
-let num = 13;
+
+let number = 13;
 let count =0;
-for (let x = 0 ; x <= 13 ; x++){
-    if(num%x==0){
+for (let x = 0 ; x <= number ; x++){
+    if(number%x==0){
         count++
     }
+}
+if(count==2){
+    console.log(number,"is prime")
+}else{
+    console.log(number,"is not prime")
+}
